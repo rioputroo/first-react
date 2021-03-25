82361ebaa6763c45a8babdab7f8c04b3a1f68bc4
Rio Trilaksono Putro <rio.trilaksono@staff.gramedia.com>
2021-03-25 11:47:40 +0700

    chore: added .prettierrc

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Person from './Person/Person';
 
 function App() {
-  const [ personsState, setPersonsState ] = useState({
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 },
     ],
     otherState: 'some other value',
-    showPersons: false
+    showPersons: false,
   });
 
   const nameChangedHandler = (event, index) => {
@@ -19,9 +19,9 @@ function App() {
     setPersonsState({
       persons: personsState.persons,
       otherState: 'some other value',
-      showPersons: true
+      showPersons: true,
     });
-  }
+  };
 
   const deletePersonHandler = (personIndex) => {
     personsState.persons.splice(personIndex, 1);
@@ -29,13 +29,13 @@ function App() {
     // updating state immutably by copy the array first
     // const persons = [...personsState.persons];
     // persons.splice(personIndex, 1);
-    
+
     setPersonsState({
       persons: personsState.persons,
       otherState: 'some other value',
-      showPersons: true
+      showPersons: true,
     });
-  }
+  };
 
   const togglePersonsHandler = () => {
     setPersonsState({
@@ -45,17 +45,17 @@ function App() {
         { name: 'Stephanie', age: 26 },
       ],
       otherState: 'some other value',
-      showPersons: !personsState.showPersons
+      showPersons: !personsState.showPersons,
     });
-  }
-  
+  };
+
   const style = {
     backgroundColor: 'white',
     font: 'inherit',
     border: '1px solid blue',
     padding: '8px',
-    cursor: 'pointer'
-  }
+    cursor: 'pointer',
+  };
 
   let persons = null;
 
@@ -63,24 +63,27 @@ function App() {
     persons = (
       <div>
         {personsState.persons.map((person, index) => {
-          return <Person 
-          click={deletePersonHandler.bind(this, index)}
-          name={person.name} 
-          age={person.age}
-          key={index}
-          changed={(event) => nameChangedHandler(event, index)} />
+          return (
+            <Person
+              click={deletePersonHandler.bind(this, index)}
+              name={person.name}
+              age={person.age}
+              key={index}
+              changed={(event) => nameChangedHandler(event, index)}
+            />
+          );
         })}
-      </div> 
-    ); 
+      </div>
+    );
   }
 
   return (
     <div className="App">
       <h1>Hi, I'm React App</h1>
       <p>This is really working</p>
-      <button 
-        style={style}
-        onClick={togglePersonsHandler}>Toggle Persons</button>
+      <button style={style} onClick={togglePersonsHandler}>
+        Toggle Persons
+      </button>
       {persons}
     </div>
   );
